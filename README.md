486060def7c06ed67f3becb13e8125b177f43a5a
gabrielSantos3 <gabrielss432112@gmail.com>
2023-05-09 20:00:31 -0300

    correções no game

---
+++ README.md
@@ -0,0 +1,70 @@
+# Caça-Níquel Online 🎰
+
+Bem-vindo ao Caça-Níquel Online! Nosso site que permite aos jogadores desfrutar da emoção de jogar em um caça-níquel virtual. Aqui você encontrará um game intuitivo e divertido, com interface responsiva (funciona corretamente em todos dispositivos) e com efeitos sonoros emocionantes. Sinta-se à vontade para explorar e aproveitar ao máximo a sua experiência de jogo!
+
+## 💻 Tecnologia Utilizada
+
+Nosso Caça-Níquel Online foi desenvolvido utilizando as seguintes tecnologias:
+
+- HTML5
+- CSS3
+- JavaScript
+
+## 🎲 Como Jogar
+
+1. Abra o site do Caça-Níquel Online em seu navegador preferido.
+2. Clique no botão "Girar" para iniciar o jogo.
+3. Aguarde os rolos pararem de girar e veja se você obteve uma combinação vencedora.
+4. Caso você vença, os seus ganhos serão automaticamente adicionados ao seu saldo.
+5. Divirta-se e boa sorte!
+
+## 💰 Gerenciamento de Saldo
+
+O Caça-Níquel Online possui um sistema de gerenciamento de saldo para permitir que os jogadores acompanhem seus ganhos e perdas. Aqui estão algumas informações importantes sobre o saldo:
+
+- O saldo inicial é de 1000 coins.
+- A cada rodada, uma determinada quantidade de coins será reduzida do saldo.
+- Os ganhos serão automaticamente adicionados ao saldo.
+- Caso o saldo chegue a 0, significa que você não possui mais coins para apostar. Recarregue o site e tente novamente!
+
+### Valor da aposta
+
+* A fim de proporcionar uma experiência de jogo equilibrada, implementamos um sistema que ajusta o valor da aposta com base no saldo atual do jogador.
+
+O valor da aposta será determinado da seguinte maneira:
+
+`aposta = Math.floor(saldo / 1000) * 100;`
+
+* Se o saldo do jogador for superior a 2000 moedas, o valor da aposta será proporcional à unidade do milhar do saldo. Por exemplo, se o saldo for 3400 moedas, a aposta será definida como 300 moedas (equivalente a 10% da casa do milhar).
+* Portanto, o valor inicial das apostas é de **100 coins**.
+* Essa abordagem garante que o valor da aposta seja ajustado de forma proporcional ao saldo do jogador, tornando-o mais justo e adequado às condições do jogo.
+
+## 🏆 Combinações Vencedoras
+
+O Caça-Níquel Online possui várias combinações vencedoras. Quanto mais símbolos iguais você conseguir, maior será o seu prêmio. Aqui estão algumas das combinações vencedoras:
+
+### Três itens
+
+|                        Item                        | Valor (coins) | Classe  |
+| :-------------------------------------------------: | ------------- | ------- |
+| ameixa, cereja, laranja, limão, maçã ou melancia | 1000          | Fruits  |
+|        Bar, ferradura, sete da sorte ou sino        | 1500          | Special |
+|          Diamante, moeda dourada ou trevo          | 2000          | Lucky   |
+
+### Dois itens
+
+|                        Item                        | Valor (coins) | Classe  |
+| :-------------------------------------------------: | ------------- | ------- |
+| ameixa, cereja, laranja, limão, maçã ou melancia | 300           | Fruits  |
+|        Bar, ferradura, sete da sorte ou sino        | 500           | Special |
+|          Diamante, moeda dourada ou trevo          | 700           | Lucky   |
+
+## 📄 Licença
+
+O Caça-Níquel Online é um projeto de código aberto sob a Licença MIT. Sinta-se à vontade para contribuir, modificar e distribuir este projeto de acordo com os termos da licença.
+
+## ℹ️ Suporte
+
+Se você tiver alguma dúvida, sugestão ou problema técnico, entre em contato com a nossa equipe de suporte pelo e-mail [gabrielss432112@gmail.com](mailto:gabrielss432112@gmail.com). Estamos aqui para ajudar!
+
+Divirta-se jogando no Caça-Níquel Online! 🎉
--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-let divGirar = document.getElementById("btnGirar");
-let qtdMoedas = document.getElementById("qtdMoedas");
-let txtsup = document.getElementById("txtsup");
+let divGirar = document.getElementById("btnGirar"); // ? Botão de girar
+let qtdMoedas = document.getElementById("qtdMoedas"); // ? h2 com o saldo
+let txtsup = document.getElementById("txtsup"); // ? Texto da div superior
 
-let imgEsq = document.getElementById("imgEsq");
-let imgMeio = document.getElementById("imgMeio");
-let imgDir = document.getElementById("imgDir");
+let imgEsq = document.getElementById("imgEsq"); // ? imagem do slot 1
+let imgMeio = document.getElementById("imgMeio"); // ? imagem do slot 2
+let imgDir = document.getElementById("imgDir"); // ? imagem do slot 1
 
-let sectionPrincipal = document.getElementById("principal");
-
-let coins = 1000;
+let coins = 1000; 
+let aposta = 100;
 
 function sortear() {
-  let sorteio = Math.floor(Math.random() * 10) + 1;
+  let sorteio = Math.floor(Math.random() * 13) + 1; 
   return sorteio;
 }
 
@@ -21,43 +20,55 @@ function mudaImagem() {
 
   switch (sorteio) {
     case 1:
-      caminho = "Imagens/img1.png";
+      caminho = "Imagens/fruits/apple.png";
       break;
 
     case 2:
-      caminho = "Imagens/img2.png";
+      caminho = "Imagens/fruits/cherry.png";
       break;
 
     case 3:
-      caminho = "Imagens/img3.png";
+      caminho = "Imagens/fruits/lemon.png";
       break;
 
     case 4:
-      caminho = "Imagens/img4.png";
+      caminho = "Imagens/fruits/orange.png";
       break;
 
     case 5:
-      caminho = "Imagens/img5.png";
+      caminho = "Imagens/fruits/plum.png";
       break;
 
     case 6:
-      caminho = "Imagens/img6.png";
+      caminho = "Imagens/fruits/watermelon.png";
       break;
 
     case 7:
-      caminho = "Imagens/img7.png";
+      caminho = "Imagens/lucky/clover.png";
       break;
 
     case 8:
-      caminho = "Imagens/img8.png";
+      caminho = "Imagens/lucky/diamond.png";
       break;
 
     case 9:
-      caminho = "Imagens/img9.png";
+      caminho = "Imagens/lucky/goldcoin.png";
       break;
 
     case 10:
-      caminho = "Imagens/img10.png";
+      caminho = "Imagens/special/bar.png";
+      break;
+
+    case 11:
+      caminho = "Imagens/special/bell.png";
+      break;
+
+    case 12:
+      caminho = "Imagens/special/horseshoe.png";
+      break;
+
+    case 13:
+      caminho = "Imagens/special/seven.png";
       break;
 
     default:
@@ -94,34 +105,60 @@ function transition() {
 }
 
 function verifica() {
-  (s1 = imgEsq.getAttribute("src")),
-    (s2 = imgMeio.getAttribute("src")),
-    (s3 = imgDir.getAttribute("src"));
 
-  if (s1 == s2 && s1 == s3) {
+  // ? pega a inicial da pasta que o item está (f, l ou s)
+  (s1 = imgEsq.getAttribute("src")),
+  (s2 = imgMeio.getAttribute("src")),
+  (s3 = imgDir.getAttribute("src")),
+  (class1 = s1.charAt(8)),
+  (class2 = s2.charAt(8)),
+  (class3 = s3.charAt(8));
+
+  // ? Verifica se pelo menos 2 itens são iguais
+  if (s1 == s2 || s1 == s3 || s2 == s3) {
     playAudioWin();
 
-    if (
-      s1 == "Imagens/img1.png" ||
-      s1 == "Imagens/img5.png" ||
-      s1 == "Imagens/img6.png" ||
-      s1 == "Imagens/img8.png" ||
-      s1 == "Imagens/img9.png" ||
-      s1 == "Imagens/img10.png"
-    ) {
-      txtsup.innerHTML = "Parabéns! + 1000 moedas";
-      coins += 1000;
-    } else if (
-      s1 == "Imagens/img2.png" ||
-      s1 == "Imagens/img3.png" ||
-      s1 == "Imagens/img7.png"
-    ) {
-      txtsup.innerHTML = "Parabéns! + 1500 moedas!";
-      coins += 1500;
+    // ? Verifica se os três são iguais
+    if (s1 == s2 && s1 == s3) {
+      if (class1 == "f") {
+        txtsup.innerHTML = "Parabéns! + 1000 moedas";
+        coins += 1000;
+      } else if (class1 == "s") {
+        txtsup.innerHTML = "Parabéns! + 1500 moedas!";
+        coins += 1500;
+      } else {
+        txtsup.innerHTML = "Parabéns! + 2000 moedas!";
+        coins += 2000;
+      }
+
+    // ? Verifica se o item 1 está entre os iguais
+    } else if (s1 == s2 || s1 == s3) {
+      if (class1 == "f") {
+        txtsup.innerHTML = "Parabéns! + 300 moedas";
+        coins += 300;
+      } else if (class1 == "s") {
+        txtsup.innerHTML = "Parabéns! + 500 moedas!";
+        coins += 500;
+      } else {
+        txtsup.innerHTML = "Parabéns! + 700 moedas!";
+        coins += 700;
+      }
+
+    // ? Como o item 1 não é igual aos outros, usa o 2
     } else {
-      txtsup.innerHTML = "Parabéns! + 2000 moedas!";
-      coins += 2000;
+      if (class2 == "f") {
+        txtsup.innerHTML = "Parabéns! + 300 moedas";
+        coins += 300;
+      } else if (class2 == "s") {
+        txtsup.innerHTML = "Parabéns! + 500 moedas!";
+        coins += 500;
+      } else {
+        txtsup.innerHTML = "Parabéns! + 700 moedas!";
+        coins += 700;
+      }
     }
+
+  // ? Não ganhou
   } else {
     txtsup.innerHTML = "Você perdeu! Tente de novo!";
   }
@@ -153,14 +190,18 @@ function rodar() {
 
   if (coins <= 0) {
     perdeu();
-  } else {
-    coins -= 100;
+  } else if(coins > 2000){
+    aposta = Math.floor(coins / 1000) * 100
+  }else{
+    aposta = 100
+  }
+    coins -= aposta;
     qtdMoedas.innerHTML = coins;
 
     playAudio();
 
     transition();
-  }
+  
 }
 
 divGirar.addEventListener("click", rodar);
