f86746238745bd2e65da1f08f5a19c2450672905
Gabriel Silva Santos <91688865+gabrielsantos7@users.noreply.github.com>
2023-07-23 10:10:37 -0300

    Fixed animation and responsiveness

--- script.js
+++ script.js
@@ -1,123 +1,109 @@
-let divGirar = document.getElementById("btnGirar"); // ? Botão de girar
-let qtdMoedas = document.getElementById("qtdMoedas"); // ? h2 com o saldo
-let txtsup = document.getElementById("txtsup"); // ? Texto da div superior
+const divGirar = document.getElementById("btnGirar"); // ? Botão de girar
+const qtdMoedas = document.getElementById("qtdMoedas"); // ? h2 com o saldo
+const txtsup = document.getElementById("txtsup"); // ? Texto da div superior
 
-let imgEsq = document.getElementById("imgEsq"); // ? imagem do slot 1
-let imgMeio = document.getElementById("imgMeio"); // ? imagem do slot 2
-let imgDir = document.getElementById("imgDir"); // ? imagem do slot 1
+const imgEsq = document.getElementById("imgEsq"); // ? imagem do slot 1
+const imgMeio = document.getElementById("imgMeio"); // ? imagem do slot 2
+const imgDir = document.getElementById("imgDir"); // ? imagem do slot 1
 
-let coins = 1000; 
+const images = {
+  1: "Imagens/fruits/apple.png",
+  2: "Imagens/fruits/cherry.png",
+  3: "Imagens/fruits/lemon.png",
+  4: "Imagens/fruits/orange.png",
+  5: "Imagens/fruits/plum.png",
+  6: "Imagens/fruits/watermelon.png",
+  7: "Imagens/lucky/clover.png",
+  8: "Imagens/lucky/diamond.png",
+  9: "Imagens/lucky/goldcoin.png",
+  10: "Imagens/special/bar.png",
+  11: "Imagens/special/bell.png",
+  12: "Imagens/special/horseshoe.png",
+  13: "Imagens/special/seven.png",
+};
+
+let coins = 1000;
 let aposta = 100;
 
 function sortear() {
-  let sorteio = Math.floor(Math.random() * 13) + 1; 
-  return sorteio;
+  return Math.floor(Math.random() * 13) + 1;
 }
 
 function mudaImagem() {
   let sorteio = sortear();
-  let caminho;
+  return images[sorteio];
+}
 
-  switch (sorteio) {
-    case 1:
-      caminho = "Imagens/fruits/apple.png";
-      break;
+function mudaHTMLAnimacao() {
+  txtsup.innerHTML = "Girando...";
+  divGirar.style.cursor = "not-allowed";
+  divGirar.removeEventListener("click", rodar);
 
-    case 2:
-      caminho = "Imagens/fruits/cherry.png";
-      break;
+  divGirar.setAttribute("id", "btnRodando");
+}
 
-    case 3:
-      caminho = "Imagens/fruits/lemon.png";
-      break;
+function mudaCorGanhou(){
+  txtsup.style.backgroundColor = "#3fcc42";
+}
 
-    case 4:
-      caminho = "Imagens/fruits/orange.png";
-      break;
-
-    case 5:
-      caminho = "Imagens/fruits/plum.png";
-      break;
-
-    case 6:
-      caminho = "Imagens/fruits/watermelon.png";
-      break;
-
-    case 7:
-      caminho = "Imagens/lucky/clover.png";
-      break;
-
-    case 8:
-      caminho = "Imagens/lucky/diamond.png";
-      break;
-
-    case 9:
-      caminho = "Imagens/lucky/goldcoin.png";
-      break;
-
-    case 10:
-      caminho = "Imagens/special/bar.png";
-      break;
-
-    case 11:
-      caminho = "Imagens/special/bell.png";
-      break;
-
-    case 12:
-      caminho = "Imagens/special/horseshoe.png";
-      break;
-
-    case 13:
-      caminho = "Imagens/special/seven.png";
-      break;
-
-    default:
-      break;
-  }
-
-  return caminho;
+function mudaCorPerdeu(){
+  txtsup.style.backgroundColor = "goldenrod";
 }
 
 function transition() {
-  let transition = setInterval(function () {
-    txtsup.innerHTML = "Girando...";
-    divGirar.style.cursor = "not-allowed";
-    divGirar.removeEventListener("click", rodar);
-
-    divGirar.setAttribute("id", "btnRodando");
+  let transition1 = setInterval(function () {
+    mudaHTMLAnimacao();
 
     imgEsq.setAttribute("src", mudaImagem());
     imgMeio.setAttribute("src", mudaImagem());
     imgDir.setAttribute("src", mudaImagem());
   }, 100);
 
-  setTimeout(function () {
-    clearInterval(transition);
+  setTimeout(() => {
+    clearInterval(transition1); // Para a primeira animação
+    let transition2 = setInterval(function () {
+      mudaHTMLAnimacao();
 
-    divGirar.style.cursor = "pointer";
+      imgMeio.setAttribute("src", mudaImagem());
+      imgDir.setAttribute("src", mudaImagem());
+    }, 100);
 
-    verifica();
+    setTimeout(() => {
+      clearInterval(transition2); // Para a segunda animação
+      let transition3 = setInterval(function () {
+        mudaHTMLAnimacao();
 
-    divGirar.setAttribute("id", "btnGirar");
+        imgDir.setAttribute("src", mudaImagem());
+      }, 100);
 
-    divGirar.addEventListener("click", rodar);
-  }, 2850);
+      setTimeout(() => {
+        clearInterval(transition3); // Para a terceira animação
+        divGirar.style.cursor = "pointer";
+
+        verifica();
+
+        divGirar.setAttribute("id", "btnGirar");
+
+        divGirar.addEventListener("click", rodar);
+      }, 975);
+    }, 975);
+  }, 975);
+
 }
 
 function verifica() {
-
   // ? pega a inicial da pasta que o item está (f, l ou s)
   (s1 = imgEsq.getAttribute("src")),
-  (s2 = imgMeio.getAttribute("src")),
-  (s3 = imgDir.getAttribute("src")),
-  (class1 = s1.charAt(8)),
-  (class2 = s2.charAt(8)),
-  (class3 = s3.charAt(8));
+    (s2 = imgMeio.getAttribute("src")),
+    (s3 = imgDir.getAttribute("src")),
+    (class1 = s1.charAt(8)),
+    (class2 = s2.charAt(8)),
+    (class3 = s3.charAt(8));
 
   // ? Verifica se pelo menos 2 itens são iguais
   if (s1 == s2 || s1 == s3 || s2 == s3) {
     playAudioWin();
-
+    mudaCorGanhou();
     // ? Verifica se os três são iguais
     if (s1 == s2 && s1 == s3) {
       if (class1 == "f") {
@@ -131,7 +117,7 @@ function verifica() {
         coins += 2000;
       }
 
-    // ? Verifica se o item 1 está entre os iguais
+      // ? Verifica se o item 1 está entre os iguais
     } else if (s1 == s2 || s1 == s3) {
       if (class1 == "f") {
         txtsup.innerHTML = "Parabéns! + 300 moedas";
@@ -144,7 +130,7 @@ function verifica() {
         coins += 700;
       }
 
-    // ? Como o item 1 não é igual aos outros, usa o 2
+      // ? Como o item 1 não é igual aos outros, usa o 2
     } else {
       if (class2 == "f") {
         txtsup.innerHTML = "Parabéns! + 300 moedas";
@@ -158,9 +144,9 @@ function verifica() {
       }
     }
 
-  // ? Não ganhou
+    // ? Não ganhou
   } else {
-    txtsup.innerHTML = "Você perdeu! Tente de novo!";
+    txtsup.innerHTML = "Não foi dessa vez...";
   }
 
   qtdMoedas.innerHTML = coins;
@@ -175,7 +161,6 @@ function perdeu() {
   } else {
     window.location.href = "index.html";
   }
-
 }
 
 function playAudio() {
@@ -190,22 +175,21 @@ function playAudioWin() {
 
 function rodar() {
   divGirar.innerHTML = "Girar";
+  mudaCorPerdeu();
 
   if (coins <= 0) {
     perdeu();
-  
-  } else if(coins > 2000){
-    aposta = Math.floor(coins / 1000) * 100
-  }else{
-    aposta = 100
+  } else if (coins > 2000) {
+    aposta = Math.floor(coins / 1000) * 100;
+  } else {
+    aposta = 100;
   }
-    coins -= aposta;
-    qtdMoedas.innerHTML = coins;
+  coins -= aposta;
+  qtdMoedas.innerHTML = coins;
 
-    playAudio();
+  playAudio();
 
-    transition();
-  
+  transition();
 }
 
 divGirar.addEventListener("click", rodar);
